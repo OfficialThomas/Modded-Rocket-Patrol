a46ce0252946966ef724b8497dafb761513c70f8
ThomasN <thomasn@ucsc.edu>
2021-06-28 13:56:18 -0700

    added fire on mouse left click

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -24,7 +24,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
             }
         }
         //fire button
-        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring){
+        if(game.input.mousePointer.buttons == 1 && !this.isFiring){
             this.isFiring = true;
             this.sfxRocket.play();      //play sfx
         }
